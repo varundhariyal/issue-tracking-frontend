5a83a9be96404ae67c8a57fbe82487efce0c7f08
varundhariyal <varundhariyal@gmail.com>
2019-06-18 11:41:48 +0530

    second change

--- src/app/error-handler/error-handler.module.ts
+++ src/app/error-handler/error-handler.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundViewComponent } from './not-found-view/not-found-view.component';
+import { ServerErrorViewComponent } from './server-error-view/server-error-view.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFoundViewComponent, ServerErrorViewComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/error-handler/server-error-view/server-error-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-error-view',
+  templateUrl: './server-error-view.component.html',
+  styleUrls: ['./server-error-view.component.css']
+})
+export class ServerErrorViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
